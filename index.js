6a0c4d087f7ee6bd75ee02d7bbf2d82034052bde
waridrox <mohdwarid4@gmail.com>
2022-01-22 14:22:50 +0530

    Airdrop func added

--- index.js
+++ index.js
@@ -27,3 +27,17 @@ const getWalletBalance = async () => {
         console.log(err);
     }
 };
+
+const airDropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+        const fromAirDropSignature = await connection.requestAirdrop(
+            new PublicKey(walletKeyPair.publicKey),
+            2 * LAMPORTS_PER_SOL
+        );
+        await connection.confirmTransaction(fromAirDropSignature);
+    } catch (err) {
+        console.log(err);
+    }
+};
