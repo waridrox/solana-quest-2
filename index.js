b83003669074833e83a4106766bdcf519b8c7318
waridrox <mohdwarid4@gmail.com>
2022-01-22 14:22:10 +0530

    Getting the wallet balance

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ console.log(publicKey);
 
 const secretKey = newPair._keypair.secretKey;
 console.log(secretKey);
+
+const getWalletBalance = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const myWallet = await Keypair.fromSecretKey(secretKey);
+        const walletBalance = await connection.getBalance(
+            new PublicKey(myWallet.publicKey)
+        );
+        console.log(`Wallet balance: ${walletBalance}`);
+    } catch (err) {
+        console.log(err);
+    }
+};
